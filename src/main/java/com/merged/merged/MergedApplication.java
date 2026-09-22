670a918325b51a7ca74f9096cf136e194d8bc271
Esha Markam <esha21101@iiitnr.edu.in>
2026-09-22 23:53:34 +0530

    initial project setup with working test endpoint

---
+++ src/main/java/com/merged/merged/MergedApplication.java
@@ -0,0 +1,23 @@
+package com.merged.merged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@SpringBootApplication
+@RestController 
+public class MergedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergedApplication.class, args);
+	}
+
+
+	@GetMapping("/api/hello")
+	public String hello() {
+		return "Merged backend is alive!";
+	}
+}
---
+++ src/test/java/com/merged/merged/MergedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merged.merged;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
